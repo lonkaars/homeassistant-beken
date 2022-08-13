6b7c8006a60e4d75706f033bae562b6c804d5bcd
lonkaars <loek@pipeframe.xyz>
2022-08-13 16:55:47 +0200

    fix NoneType error

--- light.py
+++ light.py
@@ -92,7 +92,7 @@ class BekenLight(LightEntity):
 
     transition = kwargs.get(ATTR_TRANSITION)
     if transition != None:
-      self.interpolate(brightness_old if on_old else 0, brightness, rgb_old if on_old else (0, 0, 0,), rgbw[0:3], w_old if on_old else 0, rgbw[3], transition)
+      self.interpolate(brightness_old if on_old else 0, self._brightness, rgb_old if on_old else (0, 0, 0,), self._rgb, w_old if on_old else 0, self._w, transition)
 
     self.update_beken_lamp()
 
